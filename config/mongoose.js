b151ce0b6bb362a7a085965eca74cb8fb1b47b92
dhumalkomal <107318301+dhumalkomal@users.noreply.github.com>
2023-10-29 15:27:00 +0530

    complete Project

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set("strictQuery", false);
 
-// const mongoURL = 'mongodb://127.0.0.1:27017/Habit_tracker'; 
+
 const mongoURL = 'mongodb+srv://habit:habit123@cluster0.krqzlcy.mongodb.net/habits-tracker?retryWrites=true&w=majority'; 
 
 
